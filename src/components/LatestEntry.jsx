6ed39d19c798e8eada14571a1a46f474f89c9a10
hima1222 <himarasamaranayake@gmail.com>
2025-03-07 22:02:37 +0530

    title Icon

--- src/components/LatestEntry.jsx
+++ src/components/LatestEntry.jsx
@@ -6,7 +6,7 @@ const LatestEntry = () => {
       <div className="column">
         <h2 className="title">Latest Diary Title</h2>
         <p className="description">A short description of the latest diary entry goes here.</p>
-        <a href="#" className="read-button">Read</a>
+        <a href="/Write" className="read-button">Read</a>
       </div>
       <div className="column">
         <p className="overview">This is an overview of the latest diary entry. It provides a quick summary or glimpse into the entry content. You can customize this text to match the actual diary content.</p>
